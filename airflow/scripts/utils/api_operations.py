5d0c83eb907830a05423c53ec01581b18409769f
ujeongoh <yujoh94@gmail.com>
2023-08-19 15:10:28 +0900

    refactor : 공통 함수 생성
    - site별 headers 가져오는 함수 추가
    - 주어진 파일경로에 json파일 생성하는 함수 추가
    - 주어진 파일경로에 isbn csv파일 생성하는 함수 추가
    - 공통 fetch_api_data 리팩토링 및 수정
    - init kakao api 데이터 수집 스크립트 생성

--- airflow/scripts/utils/api_operations.py
+++ airflow/scripts/utils/api_operations.py
@@ -2,10 +2,10 @@ from utils.connetions import get_s3_client
 import pandas as pd
 import os
 from io import StringIO
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 import requests
 import time
+import json
+import csv
 
 
 def get_isbn_list(bucket_name, object_key):
@@ -16,47 +16,105 @@ def get_isbn_list(bucket_name, object_key):
     return df['ISBN'].tolist()
 
 
+def get_naver_api_key(number) -> dict:
+    keys = {
+        i: {
+            "naver_client_id": os.environ.get(f"NAVER_CLIENT_ID_{i}"),
+            "naver_client_secret": os.environ.get(f"NAVER_CLIENT_SECRET_{i}")
+        }
+        for i in range(1, 21)
+    }
+    return keys[number]
+
+
+def get_kakao_api_key(number) -> dict:
+    keys = {
+        i: {
+            "kakao_api_key": os.environ.get(f"KAKAO_REST_API_KEY_{i}")
+        }
+        for i in range(1, 4)
+    }
+    return keys[number]
+
+
+def get_headers(site, key_num) -> dict:
+    headers = {}
+    if site == 'naver':
+        key = get_naver_api_key(key_num)
+        naver_client_id = key.get('naver_client_id')
+        naver_client_secret = key.get('naver_client_secret')
+        headers = {
+            "X-Naver-Client-Id": naver_client_id,
+            "X-Naver-Client-Secret": naver_client_secret
+        }
+    elif site == 'kakao':
+        key = get_kakao_api_key(key_num)
+        kakao_api_key = key.get('kakao_api_key')
+        headers = {
+            "Authorization": f'KakaoAK {kakao_api_key}'
+        }
+    return headers
+
+
+def save_csv_file(file_path, isbn_list):
+    directory = os.path.dirname(file_path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["ISBN"])
+        for isbn in isbn_list:
+            writer.writerow([isbn])
+
+
+def save_json_file(file_path, items):
+    directory = os.path.dirname(file_path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(items, f, ensure_ascii=False, indent=4)
+
+
 def fetch_api_data(isbn_list, site):
     url = ''
     headers = {}
     params = {}
     books = {'items': []}
 
+    # site별 header 설정
+    if site == 'naver':
+        url = "https://openapi.naver.com/v1/search/book.json"
+        naver_client_id = os.environ.get("NAVER_CLIENT_ID")
+        naver_client_secret = os.environ.get("NAVER_CLIENT_SECRET")
+        headers = {
+            "X-Naver-Client-Id": naver_client_id,
+            "X-Naver-Client-Secret": naver_client_secret
+        }
+    elif site == 'kakao':
+        url = "https://dapi.kakao.com/v3/search/book"
+        kakao_rest_api_key = os.environ.get("KAKAO_REST_API_KEY")
+        headers = {
+            "Authorization": f'KakaoAK {kakao_rest_api_key}'
+        }
+
     for i, isbn in enumerate(isbn_list):
         if site == 'naver' and len(isbn) == 10:
             continue
 
         if site == 'naver':
-            url = "https://openapi.naver.com/v1/search/book.json"
-            naver_client_id = os.environ.get("NAVER_CLIENT_ID")
-            naver_client_secret = os.environ.get("NAVER_CLIENT_SECRET")
-            headers = {
-                "X-Naver-Client-Id": naver_client_id,
-                "X-Naver-Client-Secret": naver_client_secret
-            }
             params = {
                 "query": isbn,
                 "start": '1'
             }
         elif site == 'kakao':
-            url = "https://dapi.kakao.com/v3/search/book"
-            kakao_rest_api_key = os.environ.get("KAKAO_REST_API_KEY")
-            headers = {
-                "Authorization": f'KakaoAK {kakao_rest_api_key}'
-            }
             params = {
                 "query": isbn,
                 "target": "isbn"
             }
-
-        # 최대 3번까지 재시도, 간격은 1초씩 증가
-        retry = Retry(total=3, backoff_factor=1)
-
-        # 재시도를 하기 위한 http 연결 관리
-        adapter = HTTPAdapter(max_retries=retry)
-
-        http = requests.Session()
-        http.mount("https://", adapter)
 
         if site == 'naver':
             time.sleep(0.2)
@@ -64,8 +122,6 @@ def fetch_api_data(isbn_list, site):
         try:
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
-
-        # connection 오류 발생 시 예외 처리
         except requests.exceptions.RequestException as e:
             print(f"Error while fetching data: {e}")
 
@@ -76,7 +132,8 @@ def fetch_api_data(isbn_list, site):
         elif site == 'kakao' and book_info['meta']['total_count'] == 0:
             print(f'{site} {i}번째 book info 없음')
             continue
-        print(f'{site} {i}번째 book info 수집')
+
         books['items'].append(book_info)
+        print(f'{site} {i}번째 book info 수집')
 
     return books
