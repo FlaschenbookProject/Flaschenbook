96148fc473fea029c425e2de4a3f891ecded2b74
ssongjj <ssong.ji.hy@gmail.com>
2023-08-26 13:29:48 +0900

    refactor : 주석 제거 및 코드 정리

--- airflow/scripts/isbn_file_to_s3.py
+++ airflow/scripts/isbn_file_to_s3.py
@@ -1,13 +1,24 @@
 import os
-from dotenv import load_dotenv
 from utils.file_operations import upload_files_to_s3
 
 
-def main():
-    load_dotenv()  # env 파일 로드
+def check_files_in_directory(directory):
+    try:
+        # 디렉토리 내의 파일 목록 얻기
+        files = os.listdir(directory)
 
+        if files:
+            print("디렉토리에 파일이 있습니다.")
+        else:
+            print("디렉토리에 파일이 없습니다.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main():
     bucket_name = os.getenv("BUCKET_NAME")
-    source_dir = 'airflow/scripts/data/isbn/'
+    source_dir = "data/isbn/"
+    check_files_in_directory(source_dir)
     upload_files_to_s3(bucket_name, source_dir)
 
 
