e8c92a0b18b0af58359141b127da492e74eca66a
ujeongoh <yujoh94@gmail.com>
2023-08-29 22:00:31 +0900

    fix : /test 경로로 들어갔던 페이지 임시로 홈화면으로 지정

---
+++ flaschenbook-react-app/src/components/Home.js
@@ -0,0 +1,32 @@
+import logo from "../logo.svg";
+import "../App.css";
+import { useEffect, useState } from "react";
+
+function Home(props) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.text())
+      .then((m) => setMessage(m));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
