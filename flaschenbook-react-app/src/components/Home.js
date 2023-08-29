011b9b920fdb2606fce5ceab2d160389b7dc8faa
ssongjj <ssong.ji.hy@gmail.com>
2023-08-30 06:18:26 +0900

    feat : Main 화면 구현 및 각 화면 연동 추가

--- flaschenbook-react-app/src/components/Home.js
+++ flaschenbook-react-app/src/components/Home.js
@@ -1,32 +1,150 @@
-import logo from "../logo.svg";
-import "../App.css";
-import { useEffect, useState } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
+import Slider from 'react-slick';
+import MainLogo from './MainLogo';
+import Footer from './Footer';
+import '../css/Logo.css'; // 전역 CSS 파일을 import
+import '../css/Font.css'; // Font.css 파일을 import
 
-function Home(props) {
-  const [message, setMessage] = useState("");
+class MainPage extends Component {
+  state = {
+    isLoggedIn: false,
+    userGenre: '',
+    userKeywords: '',
+    bestSellers: [],
+    newReleases: [],
+    trendingBooks: [],
+    popularBooks: [],
+  };
 
-  useEffect(() => {
-    fetch("/api/test")
-      .then((res) => res.text())
-      .then((m) => setMessage(m));
-  }, []);
+  componentDidMount() {
+    // 서버에서 사용자 정보 및 도서 데이터 가져오기
+    axios.get('/api/userInfo')
+      .then(response => {
+        const { isLoggedIn, genre, keywords} = response.data;
+        this.setState({ isLoggedIn, userGenre: genre, userKeywords: keywords });
+      })
+      .catch(error => {
+        console.error('Error fetching user info:', error);
+      });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    axios.get('/api/bestSellers')
+      .then(response => {
+        this.setState({ bestSellers: response.data });
+      })
+      .catch(error => {
+        console.error('Error fetching best sellers:', error);
+      });
+
+    axios.get('/api/newReleases')
+      .then(response => {
+        this.setState({ newReleases: response.data });
+      })
+      .catch(error => {
+        console.error('Error fetching new releases:', error);
+      });
+
+    axios.get('/api/trendingBooks')
+      .then(response => {
+        this.setState({ trendingBooks: response.data });
+      })
+      .catch(error => {
+        console.error('Error fetching trending books:', error);
+      });
+
+    axios.get('/api/popularBooks')
+      .then(response => {
+        this.setState({ popularBooks: response.data });
+      })
+      .catch(error => {
+        console.error('Error fetching popular books:', error);
+      });
+  }
+
+  render() {
+    const {
+      isLoggedIn,
+      userGenre,
+      userKeywords,
+      bestSellers,
+      newReleases,
+      trendingBooks,
+      popularBooks,
+    } = this.state;
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+    };
+
+    return (
+      <div className="main-page">
+        <header>
+          <div className="logo">
+            <MainLogo />
+            {/* 로고 이미지 */}
+          </div>
+        </header>
+        <main>
+          {isLoggedIn && (
+            <>
+              <section>
+                <h1>{`" 님이 선호하는 ${userGenre}" 장르의 책`}</h1>
+                <Slider {...settings}>
+                  {bestSellers.map(book => (
+                    <div key={book.id}>
+                      <img src={book.imgUrl} alt={book.title} />
+                      <p>{book.title}</p>
+                    </div>
+                  ))}
+                </Slider>
+              </section>
+              <section>
+                <h1>{`" 님을 위한 책`}</h1>
+                <Slider {...settings}>
+                  {bestSellers.map(book => (
+                    <div key={book.id}>
+                      <img src={book.thumbnailUrl} alt={book.title} />
+                      <p>{book.title}</p>
+                    </div>
+                  ))}
+                </Slider>
+              </section>
+            </>
+          )}
+
+          <section>
+            <h1>이번 주의 베스트 셀러</h1>
+            <Slider {...settings}>
+              {bestSellers.map(book => (
+                <div key={book.id}>
+                  <img src={book.imgUrl} alt={book.title} />
+                  <p>{book.title}</p>
+                </div>
+              ))}
+            </Slider>
+          </section>
+          <section>
+            <h1>이달의 신간</h1>
+            <Slider {...settings}>
+              {newReleases.map(book => (
+                <div key={book.id}>
+                  <img src={book.imgUrl} alt={book.title} />
+                  <p>{book.title}</p>
+                </div>
+              ))}
+            </Slider>
+          </section>
+        </main>
+        <div className="footer">
+            <Footer />
+          </div>
+      </div>
+    );
+  }
 }
 
-export default Home;
+export default MainPage;
---
+++ flaschenbook-react-app/src/components/MainLogo.js
@@ -0,0 +1,40 @@
+import '../css/Logo.css'; // 전역 CSS 파일을 import
+import '../css/Font.css'; // Font.css 파일을 import
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'; // React Router import
+
+// MainLogo.js
+const MainLogo = ({ isLoggedIn }) => {
+  return (
+        <div className="logo-container">
+        {/* SVG 배경 이미지 */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.3" d="M0,160L14.1,170.7C28.2,181,56,203,85,224C112.9,245,141,267,169,250.7C197.6,235,226,181,254,144C282.4,107,311,85,339,85.3C367.1,85,395,107,424,101.3C451.8,96,480,64,508,85.3C536.5,107,565,181,593,197.3C621.2,213,649,171,678,160C705.9,149,734,171,762,160C790.6,149,819,107,847,128C875.3,149,904,235,932,229.3C960,224,988,128,1016,90.7C1044.7,53,1073,75,1101,85.3C1129.4,96,1158,96,1186,80C1214.1,64,1242,32,1271,64C1298.8,96,1327,192,1355,224C1383.5,256,1412,224,1426,208L1440,192L1440,0L1425.9,0C1411.8,0,1384,0,1355,0C1327.1,0,1299,0,1271,0C1242.4,0,1214,0,1186,0C1157.6,0,1129,0,1101,0C1072.9,0,1045,0,1016,0C988.2,0,960,0,932,0C903.5,0,875,0,847,0C818.8,0,791,0,762,0C734.1,0,706,0,678,0C649.4,0,621,0,593,0C564.7,0,536,0,508,0C480,0,452,0,424,0C395.3,0,367,0,339,0C310.6,0,282,0,254,0C225.9,0,198,0,169,0C141.2,0,113,0,85,0C56.5,0,28,0,14,0L0,0Z"></path>
+            {/* SVG의 경로 및 텍스트 내용 */}
+        </svg>
+        {/* 로고 이미지 */}
+      {/* 로고 이미지 */}
+      <img src="/logo.png" alt="로고" className="logo-image" />
+      <div className={`login-controls ${isLoggedIn ? 'logged-in' : 'logged-out'}`}>
+        {isLoggedIn ? (
+          <>
+            <span>로그아웃</span>
+            <Link to="/survey" className="link-style">
+              마이페이지
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/register" className="link-style">
+              회원가입
+            </Link>
+            <Link to="/login"  className="link-style">
+              로그인
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MainLogo;
