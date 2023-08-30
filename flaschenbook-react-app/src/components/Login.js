5b6c066af0282938d19831d7df13a190d50c9cf5
ujeongoh <yujoh94@gmail.com>
2023-08-30 19:48:31 +0900

    style : header, footer 적용

--- flaschenbook-react-app/src/components/Login.js
+++ flaschenbook-react-app/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import AppContext from "../context";
 import { useNavigate } from "react-router-dom";
+import MainLogo from "./MainLogo";
+import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Login(props) {
@@ -48,51 +50,64 @@ function Login(props) {
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-center align-items-center vh-100">
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="text-center">Login</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
+    <div className="main-page">
+      <header>
+        <div className="logo">
+          <MainLogo />
+          {/* 로고 이미지 */}
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-4">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="text-center">Login</h2>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label htmlFor="email" className="form-label">
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="password" className="form-label">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
+                    {warning && (
+                      <div className="alert alert-warning" role="alert">
+                        {warning}
+                      </div>
+                    )}
+                    <button type="submit" className="btn btn-primary w-100">
+                      Login
+                    </button>
+                  </form>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                {warning && (
-                  <div className="alert alert-warning" role="alert">
-                    {warning}
-                  </div>
-                )}
-                <button type="submit" className="btn btn-primary w-100">
-                  Login
-                </button>
-              </form>
+              </div>
             </div>
           </div>
         </div>
+      </main>
+      <div className="footer">
+        <Footer />
       </div>
     </div>
   );
