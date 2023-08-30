20dc1bc770ae0a50db2d93aded01737b731a21d3
ujeongoh <yujoh94@gmail.com>
2023-08-30 21:05:35 +0900

    feat : header, footer 적용 및 저장 항목 추가
    - birthdate 항목 추가
    - 간격 조정
    - 비밀번호 보이기 아이콘 추가

--- flaschenbook-react-app/src/components/Register.js
+++ flaschenbook-react-app/src/components/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import MainLogo from "./MainLogo";
+import Footer from "./Footer";
 
 function Register(props) {
   const [username, setUsername] = useState("");
@@ -7,15 +9,10 @@ function Register(props) {
   const [gender, setGender] = useState("");
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [birthdate, setBirthdate] = useState("");
+
   const navigate = useNavigate();
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
 
   const handleEmailChange = (event) => {
     const email = event.target.value;
@@ -32,7 +29,7 @@ function Register(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const user = { username, email, password, gender };
+    const user = { username, email, password, gender, birthdate };
 
     fetch("/api/users/register", {
       method: "POST",
@@ -42,6 +39,17 @@ function Register(props) {
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
+
+        // 회원가입 성공하면 바로 로그인
+        return fetch("/api/users/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        });
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login Success:", data);
         navigate("/survey");
       })
       .catch((error) => {
@@ -64,79 +72,129 @@ function Register(props) {
   };
 
   return (
-    <div className="container d-flex align-items-center justify-content-center vh-100">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-            autocomplete="username"
-          />
+    <div className="main-page">
+      <header>
+        <div className="logo">
+          <MainLogo />
+          {/* 로고 이미지 */}
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            autocomplete="email"
-          />
-          {emailError && <div className="text-danger">{emailError}</div>}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Gender</label>
-          <br />
-          <div>
-            <label>
+      </header>
+
+      <div className="container d-flex justify-content-center">
+        <form onSubmit={handleSubmit}>
+          <h3>Sign In</h3>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              autocomplete="username"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              autocomplete="email"
+            />
+            {emailError && <div className="text-danger">{emailError}</div>}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <div className="input-group mb-3">
               <input
+                type={showPassword ? "text" : "password"}
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-outline-secondary border-left-0"
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <i className="fa fa-eye-slash"></i>
+                  ) : (
+                    <i className="fa fa-eye"></i>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="birthdate" className="form-label">
+              Birthdate
+            </label>
+            <input
+              type="date"
+              className="form-control"
+              id="birthdate"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <div>
+              <label htmlFor="gender" className="form-label">
+                Gender
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
                 type="radio"
+                name="gender"
+                id="male"
                 value="M"
                 checked={gender === "M"}
                 onChange={(e) => setGender(e.target.value)}
               />
-              Male
-            </label>
-            <label>
+              <label className="form-check-label" htmlFor="male">
+                Male
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
               <input
+                className="form-check-input"
                 type="radio"
-                id="gender"
+                name="gender"
+                id="female"
                 value="F"
                 checked={gender === "F"}
                 onChange={(e) => setGender(e.target.value)}
               />
-              Female
-            </label>
+              <label className="form-check-label" htmlFor="female">
+                Female
+              </label>
+            </div>
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary mb-3">
-          Register
-        </button>
-        <button className="btn btn-secondary" onClick={handleGoogleLogin}>
-          Start with Google Account
-        </button>
-      </form>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
