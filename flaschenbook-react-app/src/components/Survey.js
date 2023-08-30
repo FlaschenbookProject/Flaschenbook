2e50b1a2c925f3f185a0d54d6931919f53c9c5c1
ujeongoh <yujoh94@gmail.com>
2023-08-31 02:27:46 +0900

    feat : survey 화면 작업

--- flaschenbook-react-app/src/components/Survey.js
+++ flaschenbook-react-app/src/components/Survey.js
@@ -1,32 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Survey() {
-  const [sentencePairs, setSentencePairs] = useState([
-    ["The sun is shining.", "It's a beautiful day."],
-    ["I am learning React.", "Programming is fun."],
-    ["pick me.", "i'm hungry"],
-    ["great", "bored"],
-    ["literature", "SF"],
-  ]);
+  const [contentPairs, setContentPairs] = useState([""]);
   const navigate = useNavigate();
-  const [selectedSentences, setSelectedSentences] = useState([]);
+  const [selectedContents, setSelectedContents] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/survey/content-pairs")
+      .then((response) => response.json())
+      .then((data) => setContentPairs(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   const handleSelection = (index) => {
-    setSelectedSentences([...selectedSentences, sentencePairs[0][index]]);
-    setSentencePairs(sentencePairs.slice(1));
+    const selectedContent =
+      index === 0 ? contentPairs[0].content1 : contentPairs[0].content2;
+    setSelectedContents([...selectedContents, selectedContent]);
+    setContentPairs(contentPairs.slice(1));
+
+    // 마지막 선택지일 경우 선택했을 때 서버로 선택정보 보냄
+    if (contentPairs.length === 1) {
+      const sessionInfo = JSON.parse(localStorage.getItem("sessionInfo"));
+      const userId = sessionInfo.userId;
+
+      fetch("/api/survey/selected-contents", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId,
+          selectedContents: [...selectedContents, selectedContent],
+        }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          console.log("Success");
+          navigate("/");
+        })
+        .catch((error) => console.error("Error:", error));
+    }
   };
 
   return (
     <div>
-      {sentencePairs.length ? (
+      {contentPairs.length ? (
         <div>
-          <h2>Select a sentence:</h2>
+          <h2>Select a content:</h2>
           <button onClick={() => handleSelection(0)}>
-            {sentencePairs[0][0]}
+            {contentPairs[0].content1 ? contentPairs[0].content1.content : ""}
           </button>
           <button onClick={() => handleSelection(1)}>
-            {sentencePairs[0][1]}
+            {contentPairs[0].content2 ? contentPairs[0].content2.content : ""}
           </button>
         </div>
       ) : (
