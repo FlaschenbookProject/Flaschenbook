119e21b23e94a934ffdff4b1360f2a405a3e07f4
ujeongoh <yujoh94@gmail.com>
2023-09-01 04:53:26 +0900

    feat : survey 관련 모델 및 엔티티 추가

--- flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/model/SurveyContentModel.java
+++ flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/model/SurveyContentModel.java
@@ -2,11 +2,14 @@ package com.book.flaschenbook.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class SurveyContentModel {
 
     private String id;
     private String content;
     private String type;
+    private List<String> isbn;
 
 }
