82eb0da01f93d0dd289619c85bd74f1474070a89
ujeongoh <yujoh94@gmail.com>
2023-09-01 11:55:15 +0900

    fix : PK에 맞춰 인자 변경

--- flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/repository/BookDetailRepository.java
+++ flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/repository/BookDetailRepository.java
@@ -1,10 +1,11 @@
 package com.book.flaschenbook.repository;
 
 import com.book.flaschenbook.entity.BookDetailEntity;
+import com.book.flaschenbook.entity.BookDetailIdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookDetailRepository extends JpaRepository<BookDetailEntity, String> {
+public interface BookDetailRepository extends JpaRepository<BookDetailEntity, BookDetailIdEntity> {
 
 }
