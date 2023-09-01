e873c253877e11779b679d5243079d7ac2d95f61
ujeongoh <yujoh94@gmail.com>
2023-09-01 10:52:05 +0900

    feat :  오늘의 책, 연관 책 리스트 가져오기 추가

--- flaschenbook-react-app/src/components/MyPage.js
+++ flaschenbook-react-app/src/components/MyPage.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-function MyPage(props) {
+function MyPage() {
+  const [todayBook, setTodayBook] = useState([]);
+  const [relatedBooks, setRelatedBooks] = useState([]);
+  const sessionInfo = JSON.parse(localStorage.getItem("sessionInfo"));
+  const userId = sessionInfo.userId;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/my-page/today-book", {
+          params: { userId },
+        });
+        console.log(response.data);
+        setTodayBook(response.data);
+      } catch (error) {
+        console.error("Error fetching the book data", error);
+      }
+    };
+    fetchData();
+  }, [userId]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/my-page/related-books", {
+          params: { userId },
+        });
+        console.log(response.data);
+        setRelatedBooks(response.data);
+      } catch (error) {
+        console.error("Error fetching the book data", error);
+      }
+    };
+    fetchData();
+  }, [userId]);
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 mx-auto text-center">
           <img
-            src="book-cover.jpg"
+            src={todayBook.imageUrl}
             alt="Book Cover"
             className="img-thumbnail"
           />
-          <h2>Book Title</h2>
-          <p>Author: Author Name</p>
-          <p>Summary: This is the summary of the book.</p>
+          <h2>{todayBook.title}</h2>
+          <p>Author: {todayBook.author}</p>
+          <p>Summary: {todayBook.description}</p>
           <p>
             Purchase: You can purchase this book <a href="#">here</a>.
           </p>
@@ -22,72 +58,20 @@ function MyPage(props) {
       <div className="row mt-5">
         <div className="col-md-12">
           <h3 className="text-center">Related Books</h3>
-          <div
-            id="carouselExampleControls"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div className="carousel-item active">
+          <div style={{ whiteSpace: "nowrap", overflowX: "auto" }}>
+            {relatedBooks.map((book, index) => (
+              <div
+                style={{ display: "inline-block", margin: "10px" }}
+                key={index}
+              >
                 <img
-                  src="related-book1.jpg"
-                  className="d-block mx-auto"
-                  alt="Related Book 1"
+                  src={book.imageUrl}
+                  className="img-fluid"
+                  alt={book.title}
+                  style={{ maxWidth: "200px", maxHeight: "300px" }}
                 />
               </div>
-              <div className="carousel-item">
-                <img
-                  src="related-book2.jpg"
-                  className="d-block mx-auto"
-                  alt="Related Book 2"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="related-book3.jpg"
-                  className="d-block mx-auto"
-                  alt="Related Book 3"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="related-book4.jpg"
-                  className="d-block mx-auto"
-                  alt="Related Book 4"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="related-book5.jpg"
-                  className="d-block mx-auto"
-                  alt="Related Book 5"
-                />
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+            ))}
           </div>
         </div>
       </div>
