19ea18f3559326e147f219b4134654e2353a163d
ujeongoh <yujoh94@gmail.com>
2023-09-01 11:03:22 +0900

    feat : 새 카테고리 아이디로 연관 도서 찾는 메소드 추가

--- flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/repository/BookInfoRepository.java
+++ flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/repository/BookInfoRepository.java
@@ -2,9 +2,19 @@ package com.book.flaschenbook.repository;
 
 import com.book.flaschenbook.entity.BookInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookInfoRepository extends JpaRepository<BookInfoEntity, String> {
-    BookInfoEntity findByIsbn(String contentId);
+    BookInfoEntity findByIsbn(String isbn);
+
+    @Query("SELECT b " +
+            "FROM BookInfoEntity b " +
+            "JOIN CategoryRelationEntity c ON b.categoryId = c.oldCategoryId " +
+            "WHERE c.newCategoryId IN :newCategoryIds")
+    List<BookInfoEntity> findByNewCategoryIds(@Param("newCategoryIds") List<Integer> newCategoryIds);
 }
