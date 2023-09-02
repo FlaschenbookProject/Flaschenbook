500d3676ef3d225d6fe849853835d65176c5deef
ujeongoh <yujoh94@gmail.com>
2023-09-03 00:11:28 +0900

    fix : 엔티티에 PK 설정

--- flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/entity/SurveySummaryEntity.java
+++ flaschenbook-spring-app/flaschenbook/src/main/java/com/book/flaschenbook/entity/SurveySummaryEntity.java
@@ -1,28 +1,28 @@
 package com.book.flaschenbook.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "SurveySummary")
+@IdClass(SurveySummaryKey.class)
 public class SurveySummaryEntity {
 
     @Id
     private int surveyId;
 
+    @Id
     private int userId;
 
+    @Id
     @Column(length = 13)
     private String contentId;
 
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String content;
-
+    @Id
     @Column(length = 1, nullable = false)
     private String type;
 
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String content;
 }
