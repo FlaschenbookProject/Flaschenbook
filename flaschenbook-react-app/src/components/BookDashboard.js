834599ab67ee514ef9bf0e8650ff375c6aa01e7c
ssongjj <ssong.ji.hy@gmail.com>
2023-09-04 08:27:01 +0900

    feat : 대시보드 이미지 추가

--- flaschenbook-react-app/src/components/BookDashboard.js
+++ flaschenbook-react-app/src/components/BookDashboard.js
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/Font.css"; // Font.css 파일을 import
 import "../css/Survey.css";
 
 function BookDashboard() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 mx-auto text-center">
-          <h3 className="survey-question-text">
-            이런 도서를 선호해요
-          </h3>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="mx-auto text-center justify-content-center">
+            <h3 className="survey-question-text">
+            </h3>
+            <img
+              src="flaschenbook_dashboard.jpg" 
+              alt="Flaschenbook Dashboard"
+              className="img-fluid" 
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default BookDashboard;
+    );
+  }
+  
+  export default BookDashboard;
