1211f6b0ab715aedb46cc7992d5ab2fb2c1596c2
ujeongoh <yujoh94@gmail.com>
2023-09-15 14:59:45 +0900

    feat : aws lambda function 작성

--- lambda/raw_to_cleaned/transform_data.py
+++ lambda/raw_to_cleaned/transform_data.py
@@ -13,12 +13,20 @@ def format_date(date_str, input_format, output_format):
 
 def transform_data(content, source):
     dict_content = json.loads(content)
-    if source == 'naver':
-        items = [entry['items'][0] for entry in dict_content['items']]
-    elif source == 'kakao':
-        items = [entry['documents'][0] for entry in dict_content['items']]
-    else:  # source == 'aladin'
-        items = [entry['item'][0] for entry in dict_content['items']]
+
+    try:
+        if source == 'naver':
+            print("source naver start")
+            items = [entry['items'][0] for entry in dict_content['items']
+                     if entry.get("items") and len(entry['items']) > 0]
+        elif source == 'kakao':
+            items = [entry['documents'][0] for entry in dict_content['items'] if entry.get(
+                "documents") and len(entry['documents']) > 0]
+        else:  # source == 'aladin'
+            items = [entry['item'][0] for entry in dict_content['items']
+                     if entry.get("item") and len(entry['item']) > 0]
+    except Exception as e:
+        raise e
 
     transformed_data = []
 
@@ -33,17 +41,24 @@ def transform_data(content, source):
             if raw_key in item:
                 if source == 'naver' and raw_key == 'author':
                     item[raw_key] = item[raw_key].replace("^", ", ")
+                    print(item[raw_key])
                 elif source == 'naver' and raw_key == 'pubdate':
                     item[raw_key] = format_date(
                         item[raw_key], "%Y%m%d", "%Y-%m-%d")
-                elif source == 'kakao' and raw_key == 'authors':
-                    item[raw_key] = ", ".join(item[raw_key])
-                elif source == 'kakao' and raw_key == 'translators':
+                    print(item[raw_key])
+                elif source == 'kakao' and (raw_key == 'authors' or raw_key == 'translators'):
+                    if not item.get(raw_key):
+                        item[raw_key] = ""
+                        continue
                     item[raw_key] = ", ".join(item[raw_key])
                 elif source == 'kakao' and raw_key == 'datetime':
-                    item[raw_key] = item[raw_key][:10]
-                elif source == 'kakao' and raw_key == 'isbn':
-                    item[raw_key] = item[raw_key].split()[1]
+                    try:
+                        item[raw_key] = item[raw_key][:10]
+                    except:
+                        print(f"인덱스 에러{item[raw_key]}")
+                if source == 'kakao' and raw_key == 'isbn':
+                    item[raw_key] = item[raw_key].strip() if item[raw_key].startswith(
+                        " ") else item[raw_key].split(" ")[1]
                 elif source == 'aladin' and raw_key == 'isbn':
                     continue
                 transformed_item[cleaned_key] = item[raw_key]
