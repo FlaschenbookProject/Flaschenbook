a5e8db5b0496a7b2d5c5748e5efa1765767b7d50
ujeongoh <yujoh94@gmail.com>
2023-09-23 20:56:00 +0900

    feat : prometheus, grafana 리소스 추가

--- flaschenbook-spring-app/terraform/ecs_alb_listener.tf
+++ flaschenbook-spring-app/terraform/ecs_alb_listener.tf
@@ -24,3 +24,25 @@ resource "aws_lb_listener" "ecs-flb-backend-alb-listener" {
   }
 }
 
+resource "aws_lb_listener" "ecs-flb-prometheus-alb-listener" {
+  load_balancer_arn = aws_lb.ecs-flb-prometheus-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs-flb-prometheus-tg.arn
+  }
+}
+
+resource "aws_lb_listener" "ecs-flb-grafana-alb-listener" {
+  load_balancer_arn = aws_lb.ecs-flb-grafana-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs-flb-grafana-tg.arn
+  }
+}
+
